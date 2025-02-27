30f85d84eb28dbeef6dacc1a363a626154a431d8
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-02-27 21:21:33 +0200

    database connection

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const connectDB = async () => {   
+  try {
+    
+    const conn = await mongoose.connect(process.env.MONGO_URI, {autoIndex: true});
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    return conn;
+
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
