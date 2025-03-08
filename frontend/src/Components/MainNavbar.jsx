200c603f3e1fd857bd99d009ca3b75cd305f8154
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-08 20:46:05 +0200

    made MainBar into a separate component

---
+++ frontend/src/Components/MainNavbar.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { AuthContext } from "./AuthProvider";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Link, NavLink } from 'react-router-dom';
+import Stack from "react-bootstrap/Stack";
+const MainNavbar = () => {
+    const { user, setUser, removeToken } = useContext(AuthContext); 
+
+    const handleLogout = () => {
+        removeToken(); 
+        setUser(null); 
+    }
+
+    return(
+        <Navbar sticky="top" bg="dark" variant="dark" expand="lg" className="w-100 p-2 px-3">
+            <Stack direction="horizontal" className="d-flex justify-content-between w-100">
+
+                <Navbar.Brand as={Link} to="/"> NewsWebApp </Navbar.Brand>
+            
+                {user ? (
+                    <>
+                    <Navbar.Text className="text-light px-3">@{user?.username}</Navbar.Text>
+                    <Nav.Link className="text-light" as={Link} to="/" onClick={handleLogout}>Logout</Nav.Link>
+                    </>
+                ) : (
+                    <Nav.Link className="text-light" as={NavLink} to="/login">Login</Nav.Link>
+                )}
+            </Stack>
+        </Navbar>
+    );
+}
+
+export default MainNavbar;
