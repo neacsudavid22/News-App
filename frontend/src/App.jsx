cd81493488e5e8d585a76336d60ee8254e3befc9
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-14 22:15:37 +0200

    added bootstrap icons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import LoginPage from "./Pages/LoginPage";
 import HomePage from "./Pages/HomePage";
 import { AuthProvider } from "./Components/AuthProvider"; 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import ArticleEditorPage from "./Pages/ArticleEditorPage";
 import ArticleUploadPage from "./Pages/ArticleUploadPage";
+import ArticlePage from "./Pages/ArticlePage";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/author" element={<ArticleEditorPage />} />
           <Route path="/upload" element={<ArticleUploadPage />} />
+          <Route path="/article/:id" element={<ArticlePage />} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
