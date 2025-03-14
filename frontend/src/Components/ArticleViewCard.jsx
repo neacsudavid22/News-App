8d45caf32c56f3248205e0a8357129886ccb5668
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-14 22:16:26 +0200

    refactored ArticleViewCard

--- frontend/src/Components/ArticleViewCard.jsx
+++ frontend/src/Components/ArticleViewCard.jsx
@@ -23,24 +23,25 @@ const ArticleViewCard = ({ article, isBig }) => {
     }, [article])
 
     return (
-        <div style={{width: "100%"}}>
-            <Card className="h-100 w-100 d-flex flex-column"> {/* Make card responsive */}
+            <Card> {/* Make card responsive */}
+ 
                 <Card.Img
                     src={backgroundPreview}
                     alt="Background"
-                    style={{ objectFit: "cover", aspectRatio: "16 / 9"}} // Image takes 70% of the card
+                    style={{ objectFit: "cover", aspectRatio: "2",  overflow: "hidden" }} // Image takes 70% of the card
                     variant='top'
                 />
+                
                 <Card.Body className='border-top d-flex flex-column justify-content-between h-100'
                 style={{ }} >
-                    <Card.Title as={isBig ? "h2" : "h3"} className='mb-3'>{article.title}</Card.Title>
+                    <Card.Title as={isBig ? "h2" : "h4"} className='my-2'>{article.title}</Card.Title>
                     
-                    { isBig && <Card.Text>
-                                {article.articleContent[0].content.toString().slice(0, 200) + "..."}
+                    { <Card.Text>
+                                {article.articleContent[0].content.toString().slice(0, isBig ? 100 : 200) + "..."}
                             </Card.Text> }
                 </Card.Body>
             </Card>
-        </div>
+    
     );
 };
 
