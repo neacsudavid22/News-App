1dfa16b6ee9df60982edc03c4bb70d5c8bb1e3f7
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-03-31 15:48:19 +0300

    Added user menu and modal for adding friends

--- frontend/src/Components/MainNavbar.jsx
+++ frontend/src/Components/MainNavbar.jsx
@@ -1,30 +1,123 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "./AuthProvider";
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
 import Stack from "react-bootstrap/Stack";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import SplitButton from 'react-bootstrap/SplitButton';
+import { sendFriendRequestById, sendFriendRequestByUsername } from "../Services/userService";
+
 const MainNavbar = () => {
     const { user, setUser, removeToken } = useContext(AuthContext); 
+    const [friendRequest, setFriendRequest] = useState({});
+    const [friendId, setFriendId] = useState(null);
+    const [friendUsername, setFriendUsername] = useState(null);
+
+    const [IS_BY_ID, SET_IS_BY_ID] = useState(true);
 
     const handleLogout = () => {
         removeToken(); 
         setUser(null); 
     }
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const navigate = useNavigate();
+
+    const handleProfile = () => {
+        if(user._id)
+            navigate('/profile/' + user._id);
+        return;
+    }
+
+    const sendFriendRequest = async () => {
+        const response = IS_BY_ID ? await sendFriendRequestById(user._id, friendId)
+                                : await sendFriendRequestByUsername(user._id, friendUsername);
+        console.log(IS_BY_ID ? "sendFriendRequestById" : "sendFriendRequestByUsername");
+        if(response.message){
+            setFriendRequest({ message: response.message, error: response.error });
+        }
+    }
+
     return(
-        <Navbar sticky="top" bg="dark" variant="dark" expand="lg" className="w-100 p-2 px-3">
+        <Navbar sticky="top" bg="dark" variant="dark" expand="lg" className="w-100 m-0 p-2 px-3">
             <Stack direction="horizontal" className="d-flex justify-content-between w-100">
 
                 <Navbar.Brand as={Link} to="/"> NewsWebApp </Navbar.Brand>
             
                 {user ? (
                     <Stack direction="horizontal">
-                        <Navbar.Text className="text-light px-3">@{user?.username}</Navbar.Text>
-                        <Nav.Link className="text-light" as={Link} to="/" onClick={handleLogout}>Logout</Nav.Link>
+
+                        <SplitButton
+                        variant="danger"
+                        align="end"
+                        title={('@' + user?.username) || "loading.." }
+                        id="dropdown-menu-align-end"
+                        
+                        >
+                            <Dropdown.Item type="button" variant="danger" onClick={handleShow}>Add a friend</Dropdown.Item>
+                            <Dropdown.Item type="button" variant="danger" onClick={handleProfile}>Go to profile</Dropdown.Item>
+                            <Dropdown.Divider />
+                            <Dropdown.Item type="button" as={Link} to="/" onClick={handleLogout}>Logout</Dropdown.Item>
+                        </SplitButton>
+
+                        <Modal show={show} onHide={handleClose} centered>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Add a friend!</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                                        <Form.Control type="text" className=" mb-3" 
+                                                      placeholder={`Insert friend's unique ${IS_BY_ID ? "id" : "username"}`}
+                                                      onChange={
+                                                        (e) => {
+                                                            IS_BY_ID ? setFriendId(e.target.value)
+                                                                     : setFriendUsername(e.target.value)
+                                                        }
+                                                    } />
+                                        <Form.Text className="text-muted" >
+                                        { IS_BY_ID ? "The id can be found in the profile page and must be shared before" 
+                                                  : "Introduce the username of the user you want to befriend" }
+                                        </Form.Text>
+                                        <br/><br/>
+                                        {   
+                                        friendRequest.message && 
+                                        <Form.Text style={{color: friendRequest.error ? "red" : "green"}}>
+                                        {friendRequest.message}
+                                        </Form.Text>
+                                        }
+                                       
+                                    </Form.Group>
+                                </Form>
+                            </Modal.Body>
+                            <Modal.Footer className="d-flex justify-content-between">
+                            <Form.Check
+                                    type="switch"
+                                    id="toggleAuth"
+                                    label={IS_BY_ID ? "id on" : "username on"}
+                                    checked={IS_BY_ID}
+                                    onChange={() => SET_IS_BY_ID((prev) => !prev)}
+                                />
+                                <div>
+                                <Button variant="secondary" onClick={handleClose} className="me-2">
+                                    Close
+                                </Button>
+                                <Button variant="danger" onClick={sendFriendRequest}>
+                                    Send Request
+                                </Button>
+                                </div>
+                            </Modal.Footer>
+                        </Modal>
                     </Stack>
                 ) : (
-                    <Nav.Link className="text-light" as={NavLink} to="/login">Login</Nav.Link>
+                    <Button className="text-light" variant="danger" as={NavLink} to="/login">Login</Button>
                 )}
             </Stack>
         </Navbar>
