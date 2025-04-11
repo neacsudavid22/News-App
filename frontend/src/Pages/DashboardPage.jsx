58fb38cdcfe8d29a4066ed6ca6dfc806dcef5e1d
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-04-11 15:20:06 +0300

    added minimal dashboard

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -1,14 +1,47 @@
 import { useContext } from "react";
 import { AuthContext } from "../Components/AuthProvider";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import MainNavbar from "../Components/MainNavbar";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router";
 
-const DashboardPage = () => {
+const DashboardPage = () => {   
 
     const {user} = useContext(AuthContext);
+    const navigation = useNavigate();
+
+    const handleCreateArticle = () => {
+        navigation("/author");
+    }
 
     return(
-    <>
-        <h1>{user && "Welcome " + user?.name + ", you are registered as " + user?.account} + "!"</h1>
-    </>
+    <div className="vh-100 bg-light">
+    <MainNavbar/>
+    <Container fluid className="d-flex justify-content-center">
+        <Row className="w-100 justify-content-center">
+            <Col xs={12} sm={12} md={8} lg={8} xl={8}>
+            <Stack gap={5} className="p-5 my-4 bg-white shadow rounded-3">
+                <h1 className="fs-2">{user && "Welcome " + user?.name + ", you are registered as " + user?.account + "!"}</h1>
+                <Stack direction="horizontal" className=" justify-content-evenly">
+                <Button size="lg" variant="primary" onClick={handleCreateArticle}>
+                    Create Article
+                </Button>
+                <Button size="lg" variant="warning">
+                    Modify Article
+                </Button>
+                { user?.account === "admin" && 
+                <Button size="lg" variant="outline-warning">
+                    Create Author Account
+                </Button> }
+                </Stack>
+            </Stack>
+            </Col>
+        </Row>
+    </Container>
+    </div>
     );
 }
 
