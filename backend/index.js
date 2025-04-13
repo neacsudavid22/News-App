1a2014f80f69957ae84e9ba91f0120f3c6f12037
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-04-13 14:43:42 +0300

    this time!

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.listen(port, () => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
