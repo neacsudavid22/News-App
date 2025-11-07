72fe9d4f59c0f9195ee436e524597840c7217afb
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-11-07 22:44:01 +0200

    Revert "Update index.js"
    This reverts commit 9d28c49d2862bf9f3f5bd5c78362ea779402eeea.

--- backend/index.js
+++ backend/index.js
@@ -43,12 +43,11 @@ app.listen(port, () => {
 });
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
-app.use(express.static(join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use((req, res) => {
-  res.sendFile(join(__dirname, "../frontend/dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
-
 
