b198a71a9d5604e2f5aceabb2e9bde83ef201aa9
neacsudavid22 <ndavid2003.dn@gmail.com>
2025-11-07 22:49:50 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,21 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
-import connectDB from './config/db.js'
-import articlesRouter from './routes/article-routes.js';
-import usersRouter from './routes/user-routes.js';
-import cloudinarydRouter from "./routes/cloudinary-routes.js";
 import cookieParser from "cookie-parser";
 import { dirname, join } from 'path'; 
 import { fileURLToPath } from "url";
-import langchainRouter from "./routes/langchain-routes.js";
+
+import connectDB from './config/db.js'
 import { connectCloudinary } from "./config/cloudinary.js";
+import articlesRouter from './routes/article-routes.js';
+import usersRouter from './routes/user-routes.js';
+import cloudinarydRouter from "./routes/cloudinary-routes.js";
+import langchainRouter from "./routes/langchain-routes.js";
 
 await connectDB();
 await connectCloudinary();
 
 const port = process.env.PORT || 5000;
-
 const app = express();
 
 app.use(cookieParser()); 
@@ -33,21 +33,19 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(join(__dirname, "../frontend/dist")));
+
 app.use('/article-api', articlesRouter);
 app.use('/user-api', usersRouter);
 app.use('/cloudinary-api', cloudinarydRouter);
 app.use('/langchain-api', langchainRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "../frontend/dist/index.html"));
+});
+
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 });
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
-
